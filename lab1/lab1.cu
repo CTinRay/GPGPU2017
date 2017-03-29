4dad4c0c688c78170a40cce3952633b6796f6999
ctinray <b03902072@ntu.edu.tw>
2017-03-29 21:06:59 +0800

    cope with boundary

--- lab1/lab1.cu
+++ lab1/lab1.cu
@@ -4,8 +4,10 @@ static const unsigned H = 768;
 static const unsigned NFRAME = 240;
 
 #define N_PARTICLES 10000
-#define SCALE 1 / 100
+#define SCALE (1.0 / 100)
 #define INIT_DISTANCE 500
+#define GRAVITY 50
+
 // typedef double2 Coord;
 // typedef double2 Velocity;
 // typedef short3* Velocity;
@@ -16,6 +18,8 @@ struct Lab1VideoGenerator::Impl {
 	int t = 0;
     double2* coordinate;
     double2* velocity;
+    double2* prev_coordinate;
+    double2* prev_velocity;
     short3* canvas;
 };
 
@@ -60,12 +64,14 @@ void rgb2yuv(short3* canvas, uint8_t* yuv) {
 }
 
 __device__ void drawDot(double2 coord, short3 color, short3* canvas) {
-    unsigned int x = coord.x * SCALE;
-    unsigned int y = coord.y * SCALE;
-    canvas[(y + 0)*W + x + 0] = color;
-    // canvas[(y + 0)*W + x + 1] = color;
-    // canvas[(y + 1)*W + x + 0] = color;
-    // canvas[(y + 1)*W + x + 1] = color;
+    int x = coord.x * SCALE + 20;
+    int y = coord.y * SCALE;
+    if ( x >= 0 && x < W - 1 && y >= 0 && y < H - 1) {
+        canvas[(y + 0)*W + x + 0] = color;
+        canvas[(y + 0)*W + x + 1] = color;
+        canvas[(y + 1)*W + x + 0] = color;
+        canvas[(y + 1)*W + x + 1] = color;
+    }
 }
 
 
@@ -85,26 +91,52 @@ void initParticles(double2* coord, double2* velocity, short3* canvas) {
 }
 
 
-__global__ void updateParticlesKernel(double2* coord, double2* velocity, short3* canvas) {
+__global__ void updateParticlesKernel(double2* prev_coord, double2* prev_velocity,
+                                      double2* coord, double2* velocity, short3* canvas) {
     const short3 WHITE = make_short3(255, 255, 255);
 
     auto i = blockDim.x * blockIdx.x + threadIdx.x;
     if (i < N_PARTICLES) {
-        // coord[i] = make_double2(x * INIT_DISTANCE, y * INIT_DISTANCE);
-        // velocity[i] = make_double2(0, 0);
+        velocity[i] = prev_velocity[i];
+        coord[i] = prev_coord[i];
+
+        // reflection
+        if (coord[i].y >= (H - 10)/SCALE) {
+            velocity[i].y = - velocity[i].y;
+        }
+        if (coord[i].x >= (W - 10)/SCALE) {
+            velocity[i].x = - velocity[i].x;
+        }
+        if (coord[i].x <= 10/SCALE) {
+            velocity[i].x = - velocity[i].x;
+        }
+
+        velocity[i].y += GRAVITY;
+        coord[i].x += velocity[i].x;
+        coord[i].y += velocity[i].y;
+
+        // boundary
+        coord[i].x = coord[i].x < 10/SCALE ? 10/SCALE : coord[i].x;
+        coord[i].x = coord[i].x > (W - 10)/SCALE ? (W - 10)/SCALE : coord[i].x;
+        coord[i].y = coord[i].y > (H - 10)/SCALE ? (H - 10)/SCALE : coord[i].y;
+        
         drawDot(coord[i], WHITE, canvas);
     }
 }
 
 
-void updateParticles(double2* coord, double2* velocity, short3* canvas) {
-    updateParticlesKernel<<<(N_PARTICLES + 31) / 32, 32>>>(coord, velocity, canvas);
+void updateParticles(double2* prev_coord, double2* prev_velocity,
+                     double2* coord, double2* velocity, short3* canvas) {
+    updateParticlesKernel<<<(N_PARTICLES + 31) / 32, 32>>>(prev_coord, prev_velocity,
+                                                           coord, velocity, canvas);
 }
 
 
 Lab1VideoGenerator::Lab1VideoGenerator(): impl(new Impl) {
     cudaMalloc(&(impl->velocity), sizeof(double2) * N_PARTICLES);
     cudaMalloc(&(impl->coordinate), sizeof(double2) * N_PARTICLES);
+    cudaMalloc(&(impl->prev_velocity), sizeof(double2) * N_PARTICLES);
+    cudaMalloc(&(impl->prev_coordinate), sizeof(double2) * N_PARTICLES);
     cudaMalloc(&(impl->canvas), sizeof(short3) * W * H * 3 / 2);
     fill(impl->canvas);
     initParticles(impl->coordinate, impl->velocity, impl->canvas);
@@ -124,9 +156,16 @@ void Lab1VideoGenerator::get_info(Lab1VideoInfo &info) {
 
 void Lab1VideoGenerator::Generate(uint8_t *yuv) {
 	// cudaMemset(yuv, (impl->t)*255/NFRAME, W*H);
-	// cudaMemset(yuv+W*H, 128, W*H/2);
-    fill(impl->canvas);
-    updateParticles(impl->coordinate, impl->velocity, impl->canvas);
-    rgb2yuv(impl->canvas, yuv);
-	++(impl->t);
+	// cudaMemset(yuv+W*H, 128, W*H/2);    
+    for (int i = 0; i < 10; ++i) {
+        fill(impl->canvas);
+        cudaMemcpy(impl->prev_coordinate, impl->coordinate,
+                   sizeof(double2) * N_PARTICLES, cudaMemcpyDeviceToDevice);
+        cudaMemcpy(impl->prev_velocity, impl->velocity,
+                   sizeof(double2) * N_PARTICLES, cudaMemcpyDeviceToDevice);
+        updateParticles(impl->prev_coordinate, impl->prev_velocity,
+                        impl->coordinate, impl->velocity, impl->canvas);    
+        rgb2yuv(impl->canvas, yuv);
+    }
+    ++(impl->t);
 }
