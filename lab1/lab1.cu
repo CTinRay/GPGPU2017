b8a2bf84cd6b5c4c05a9c79845ab1d6166d9c957
ctinray <b03902072@ntu.edu.tw>
2017-03-29 12:07:00 +0800

    drawing dots :D

--- lab1/lab1.cu
+++ lab1/lab1.cu
@@ -1,9 +1,16 @@
 #include "lab1.h"
-static const unsigned W = 640;
-static const unsigned H = 480;
+static const unsigned W = 1366;
+static const unsigned H = 768;
 static const unsigned NFRAME = 240;
 
 #define N_PARTICLES 10000
+#define SCALE 1 / 100
+#define INIT_DISTANCE 500
+// typedef double2 Coord;
+// typedef double2 Velocity;
+// typedef short3* Velocity;
+
+
 
 struct Lab1VideoGenerator::Impl {
 	int t = 0;
@@ -52,6 +59,47 @@ void rgb2yuv(short3* canvas, uint8_t* yuv) {
     rgb2yuvKernel<<<dimGrid, dimBlock>>>(canvas, yuv);    
 }
 
+__device__ void drawDot(double2 coord, short3 color, short3* canvas) {
+    unsigned int x = coord.x * SCALE;
+    unsigned int y = coord.y * SCALE;
+    canvas[(y + 0)*W + x + 0] = color;
+    // canvas[(y + 0)*W + x + 1] = color;
+    // canvas[(y + 1)*W + x + 0] = color;
+    // canvas[(y + 1)*W + x + 1] = color;
+}
+
+
+__global__ void initParticlesKernel(double2* coord, double2* velocity, short3* canvas) {
+    auto i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i < N_PARTICLES) {
+        int x = i % 100;
+        int y = i / 100;
+        coord[i] = make_double2(x * INIT_DISTANCE, y * INIT_DISTANCE);
+        velocity[i] = make_double2(0, 0);
+    }
+}
+
+
+void initParticles(double2* coord, double2* velocity, short3* canvas) {
+    initParticlesKernel<<<(N_PARTICLES + 255) / 256, 256>>>(coord, velocity, canvas);
+}
+
+
+__global__ void updateParticlesKernel(double2* coord, double2* velocity, short3* canvas) {
+    const short3 WHITE = make_short3(255, 255, 255);
+
+    auto i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i < N_PARTICLES) {
+        // coord[i] = make_double2(x * INIT_DISTANCE, y * INIT_DISTANCE);
+        // velocity[i] = make_double2(0, 0);
+        drawDot(coord[i], WHITE, canvas);
+    }
+}
+
+
+void updateParticles(double2* coord, double2* velocity, short3* canvas) {
+    updateParticlesKernel<<<(N_PARTICLES + 31) / 32, 32>>>(coord, velocity, canvas);
+}
 
 
 Lab1VideoGenerator::Lab1VideoGenerator(): impl(new Impl) {
@@ -59,6 +107,7 @@ Lab1VideoGenerator::Lab1VideoGenerator(): impl(new Impl) {
     cudaMalloc(&(impl->coordinate), sizeof(double2) * N_PARTICLES);
     cudaMalloc(&(impl->canvas), sizeof(short3) * W * H * 3 / 2);
     fill(impl->canvas);
+    initParticles(impl->coordinate, impl->velocity, impl->canvas);
 }
 
 Lab1VideoGenerator::~Lab1VideoGenerator() {}
@@ -77,6 +126,7 @@ void Lab1VideoGenerator::Generate(uint8_t *yuv) {
 	// cudaMemset(yuv, (impl->t)*255/NFRAME, W*H);
 	// cudaMemset(yuv+W*H, 128, W*H/2);
     fill(impl->canvas);
+    updateParticles(impl->coordinate, impl->velocity, impl->canvas);
     rgb2yuv(impl->canvas, yuv);
 	++(impl->t);
 }
