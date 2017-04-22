8ea78de922d984895b1c8754824fd8252e4562e5
ctinray <b03902072@ntu.edu.tw>
2017-04-22 22:16:50 +0800

    pass part 1

--- lab2/counting.cu
+++ lab2/counting.cu
@@ -10,10 +10,26 @@
 __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
 __device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
 
+
+struct one_if_not_space : public thrust::unary_function<char,int>
+{
+  __host__ __device__
+  int operator()(char c) { return c == '\n' ? 0 : 1; }
+};
+
+
 void CountPosition1(const char *text, int *pos, int text_size)
 {
+    one_if_not_space func;
+    thrust::transform(thrust::device, text, text + text_size, pos, func);    
+    thrust::inclusive_scan_by_key(thrust::device, pos, pos + text_size, pos, pos);
 }
 
 void CountPosition2(const char *text, int *pos, int text_size)
 {
+
+    // one_if_not_space func;
+    // thrust::transform(text, text + text_size, pos, func);
+    // thrust::inclusive_scan_by_key(pos, pos + text_size, pos, pos);
+
 }
