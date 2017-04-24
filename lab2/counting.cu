ff807beac76616ecf23f54fe9fa30130e201f10e
ctinray <b03902072@ntu.edu.tw>
2017-04-24 16:30:13 +0800

    count position2 simple

--- lab2/counting.cu
+++ lab2/counting.cu
@@ -25,9 +25,27 @@ void CountPosition1(const char *text, int *pos, int text_size)
     thrust::inclusive_scan_by_key(thrust::device, pos, pos + text_size, pos, pos);
 }
 
+
+__global__ void countPositionKernel(const char *text, int *pos, int text_size) {
+    auto i = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (text[i] == '\n') {
+        pos[i] = 0;
+    }
+    
+    if (i == 0 || text[i - 1] == '\n') {
+        int cnt = 0;
+        while (i + cnt < text_size && text[i + cnt] != '\n') {
+            pos[i + cnt] = cnt + 1;
+            cnt += 1;
+        }
+    }
+
+}
+
 void CountPosition2(const char *text, int *pos, int text_size)
 {
-
+    countPositionKernel<<< (text_size + 255)/ 256, 256>>>(text, pos, text_size);
     // one_if_not_space func;
     // thrust::transform(text, text + text_size, pos, func);
     // thrust::inclusive_scan_by_key(pos, pos + text_size, pos, pos);
