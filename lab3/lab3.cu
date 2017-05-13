56bbae32b1550d9acb6011845ecbe0600df4f6c0
ctinray <b03902072@ntu.edu.tw>
2017-05-13 14:34:31 +0800

    finish hierarchy acceleration

--- lab3/lab3.cu
+++ lab3/lab3.cu
@@ -2,7 +2,8 @@
 #include <cstdio>
 
 #define MIN(A, B) ((A) < (B) ? (A) : (B))
-#define N_HIER 2
+#define N_HIER 4
+#define N_ITERS 100
 
 __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
 __device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
@@ -109,20 +110,24 @@ __global__ void DownSample(const float *buf1, float *buf2,
     const int y = blockIdx.y * blockDim.y + threadIdx.y;
 	const int x = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (x < w / nColors / rate and y < h / nColors  / rate) {
+    if (x < w / rate and y < h / rate) {
         int xMax = MIN((x + 1) * rate, w);
         int yMax = MIN((y + 1) * rate, h);
         float sum[] = {0, 0, 0};
+        int n = 0;
         for (int i = x * rate; i < xMax; ++i) {
             for (int j = y * rate; j < yMax; ++j) {
                 for (int c = 0; c < nColors; ++c) {
-                    sum[c] += buf1[nColors * (x + y * w) + c];
+                    sum[c] += buf1[nColors * (i + j * w) + c];
                 }
+                n += 1;
             }
         }
         for (int c = 0; c < nColors; ++c) {
+            // buf2[nColors * (x + y * (w / rate)) + c] =
+            //     sum[c] / n;
             buf2[nColors * (x + y * (w / rate)) + c] =
-                sum[c] / (xMax - x) / (yMax - y);
+                buf1[nColors * (x * rate + y * rate * w) + c];
         }
     }    
 }
@@ -144,43 +149,6 @@ __global__ void UpSample(const float *buf1, float *buf2,
 }
 
 
-void PoissonImagecloningSub(
-	const float *background,
-	const float *target,
-	const float *mask,
-	float *output,
-	const int wb, const int hb, const int wt, const int ht,
-	const int oy, const int ox) {
-
-    // set up
-    float *fixed, *buf1, *buf2;
-    cudaMalloc(&fixed, 3*wt*ht*sizeof(float));
-    cudaMalloc(&buf1, 3*wt*ht*sizeof(float));
-    cudaMalloc(&buf2, 3*wt*ht*sizeof(float));
-
-    // initialize the iteration
-    dim3 gdim(CeilDiv(wt,32), CeilDiv(ht,16)), bdim(32,16);
-    CalculateFixed<<<gdim, bdim>>>(background, target, mask, fixed,
-                                   wb, hb, wt, ht, oy, ox);
-    cudaMemcpy(buf1, target, sizeof(float)*3*wt*ht, cudaMemcpyDeviceToDevice);
-    
-    // iterate
-    for (int i = 0; i < 10000; ++i) {
-        PoissonImageCloningIteration<<<gdim, bdim>>>(fixed, mask, buf1, buf2, wt, ht);
-        PoissonImageCloningIteration<<<gdim, bdim>>>(fixed, mask, buf2, buf1, wt, ht);
-    }
-
-    // copy the image back
-    cudaMemcpy(output, background, wb*hb*sizeof(float)*3, cudaMemcpyDeviceToDevice);
-    SimpleClone<<<gdim, bdim>>>(background, buf1, mask, output,
-                                wb, hb, wt, ht, oy, ox);
-
-    // clean up
-    cudaFree(fixed);
-    cudaFree(buf1);
-    cudaFree(buf2);
-}
-
 void PoissonImageCloning(
 	const float *background,
 	const float *target,
@@ -189,34 +157,70 @@ void PoissonImageCloning(
 	const int wb, const int hb, const int wt, const int ht,
 	const int oy, const int ox) {
 
+    cudaMemset(output, 0, 3*wb*hb*sizeof(float));
     
     float *prevBackground;
-    cudaMalloc(&prevBackground, 3*wt*ht*sizeof(float));
-    cudaMemcpy(prevBackground, background, sizeof(float)*3*wt*ht,
+    cudaMalloc(&prevBackground, 3*wb*hb*sizeof(float));
+    cudaMemcpy(prevBackground, background, sizeof(float)*3*wb*hb,
                cudaMemcpyDeviceToDevice);
+
+    // set up
+    float *buf1, *buf2;
+    cudaMalloc(&buf1, 3*wt*ht*sizeof(float));
+    cudaMalloc(&buf2, 3*wt*ht*sizeof(float));
+
     
+    dim3 gdim(CeilDiv(wt,32), CeilDiv(ht,16)), bdim(32,16);
+
+    // use target as initial
+    cudaMemcpy(buf2, target, wt*ht*sizeof(float)*3, cudaMemcpyDeviceToDevice);
+
     for (int i = 0; i < N_HIER; ++i) {
-        int rate = 1 << (N_HIER - i);
+        int rate = 1 << (N_HIER - i - 1);
+        printf("rate = %d\n", rate);
         
-        float *subBackground, *subTarget, *subMask;
-        cudaMalloc(&subBackground, 3*(wt/rate)*(ht/rate)*sizeof(float));
+        float *fixed, *subBackground, *subTarget, *subMask;
+        cudaMalloc(&subBackground, 3*wb*hb*sizeof(float));
         cudaMalloc(&subTarget, 3*(wt/rate)*(ht/rate)*sizeof(float));
         cudaMalloc(&subMask, (wt/rate)*(ht/rate)*sizeof(float));
+        cudaMalloc(&fixed, 3*wt*ht*sizeof(float));
         
-        dim3 gdim, bdim(32,16);
-        
-        gdim = dim3(CeilDiv(wb,32), CeilDiv(hb,16));
+        // Prepare background to calculate fixed
+        gdim = dim3(CeilDiv(wb,32), CeilDiv(hb,16));        
         DownSample<<<gdim, bdim>>>(prevBackground, subBackground, wb, hb, rate, 3);
-        
-        gdim = dim3(CeilDiv(wt,32), CeilDiv(ht,16)), 
-        DownSample<<<gdim, bdim>>>(target, subTarget, wt, ht, rate, 3);
-        DownSample<<<gdim, bdim>>>(mask, subMask, wt, ht, rate, 1);
-        
-        PoissonImagecloningSub(subBackground, subTarget, subMask, output,
-                               wb, hb, wt, ht, oy, ox);
 
+        // Prepare subMask, subTarget to calculate fixed
+        gdim = dim3(CeilDiv(wt,32), CeilDiv(ht,16)), 
+        DownSample<<<gdim, bdim>>>(mask, subMask, wt, ht, rate, 1);
+        DownSample<<<gdim, bdim>>>(target, subTarget, wt, ht, rate, 3);
+        CalculateFixed<<<gdim, bdim>>>(subBackground, subTarget, subMask, fixed,
+                                       wb/rate, hb/rate, wt/rate, ht/rate, oy/rate, ox/rate);
+
+        // DownSample from previous result and store in buf1
+        DownSample<<<gdim, bdim>>>(buf2, buf1, wt, ht, rate, 3);
+
+        // Do iteration
+        for (int i = 0; i < N_ITERS; ++i) {
+            PoissonImageCloningIteration<<<gdim, bdim>>>(fixed, subMask, buf1, buf2, wt/rate, ht/rate);
+            PoissonImageCloningIteration<<<gdim, bdim>>>(fixed, subMask, buf2, buf1, wt/rate, ht/rate);
+        }
+        
+        // UpSample result and keep it in buf2
         gdim = dim3(CeilDiv(wb,32), CeilDiv(hb,16));
-        UpSample<<<gdim, bdim>>>(output, prevBackground, wb, hb, rate, 3);
+        UpSample<<<gdim, bdim>>>(buf1, buf2, wt, ht, rate, 3);
+        
+        cudaFree(subBackground);
+        cudaFree(subTarget);
+        cudaFree(subMask);
+        cudaFree(fixed);
     }
+
+    // copy the image back
+    cudaMemcpy(output, background, wb*hb*sizeof(float)*3, cudaMemcpyDeviceToDevice);
+    SimpleClone<<<gdim, bdim>>>(background, buf2, mask, output,
+                                wb, hb, wt, ht, oy, ox);
     
+    // clean up
+    cudaFree(buf1);
+    cudaFree(buf2);    
 }
